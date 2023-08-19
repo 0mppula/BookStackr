8bfb9f849d7a06a8d3b060993d0274808a0ee342
Omar Kraidié <omppula_1@hotmail.com>
2023-08-19 12:33:49 +0300

    Added footer social icons.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,54 @@
 import { FC } from 'react';
+import { FaGithub, FaLinkedin, FaTwitter, FaEnvelope } from 'react-icons/fa';
 
 const Footer: FC = () => {
 	return (
 		<footer>
-			<span>Developed by </span>
-			<a href="https://www.omarkraidie.com/" target="_blank" rel="noopener noreferrer">
-				Omar Kraidié
-			</a>
+			<div className="footer-links">
+				<a
+					href="https://github.com/0mppula"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="Github"
+				>
+					<FaGithub className="footer-icon" />
+				</a>
+
+				<a
+					href="mailto:devomarkraidie@gmail.com"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="Email"
+				>
+					<FaEnvelope className="footer-icon" />
+				</a>
+
+				<a
+					href="https://www.linkedin.com/in/omarkraidie/"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="Linkedin"
+				>
+					<FaLinkedin className="footer-icon" />
+				</a>
+				<a
+					href="https://twitter.com/OmarKraidie"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="Twitter"
+				>
+					<FaTwitter className="footer-icon" />
+				</a>
+			</div>
+
+			<hr />
+
+			<div>
+				<span>Developed by </span>
+				<a href="https://www.omarkraidie.com/" target="_blank" rel="noopener noreferrer">
+					Omar Kraidié
+				</a>
+			</div>
 		</footer>
 	);
 };
